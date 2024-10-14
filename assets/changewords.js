2565ee6af29d6961bf59b23890d315fe2b442e00
samael <mrrobot7380@gmail.com>
2024-10-14 16:17:52 +0300

    Added typerwriter effect

---
+++ assets/changewords.js
@@ -0,0 +1,14 @@
+const words = ["Event", "Journey", "Anniversary", "Special Day", "Wedding"];
+let currentIndex = 0;
+const textElement = document.querySelector(".text-4");
+
+function changeWord() {
+  textElement.textContent = words[currentIndex]; // Immediately update the word
+  currentIndex = (currentIndex + 1) % words.length; // Move to the next word for the next cycle
+
+  setTimeout(() => {
+    // This will delay the next cycle, making the effect smoother
+  }, 3500); // 3s delay to match halfway point of animation
+}
+
+setInterval(changeWord, 5000); // Change word every 5 seconds
