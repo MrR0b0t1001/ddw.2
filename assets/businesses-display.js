5d093fc2dadd550149e91edf135f28e4b95ac614
samael <mrrobot7380@gmail.com>
2024-10-24 23:20:02 +0300

    Added functionality to navbar in services.html

--- assets/businesses-display.js
+++ assets/businesses-display.js
@@ -1,4 +1,4 @@
-const selectBusiness = () => {
+const displayBusinesses = () => {
   // Map the selectors to the business types
   const businessSelectors = {
     ".business.florists": ".vendor.florists",
@@ -16,19 +16,15 @@ const selectBusiness = () => {
     //
     //
     const businessCards = document.querySelectorAll(selector);
-    businessCards.forEach((card) => {
-      card.addEventListener("click", () => {
-        document.querySelectorAll(".business").forEach((businessCard) => {
-          businessCard.classList.add("hidden");
-          businessCard.classList.remove("visible");
-        });
+    businessCards.forEach(() => {
+      document.querySelectorAll(".business").forEach((businessCard) => {
+        businessCard.classList.add("visible");
+        businessCard.classList.remove("hidden");
+      });
 
-        document
-          .querySelectorAll(businessTypeSelector)
-          .forEach((vendorCard) => {
-            vendorCard.classList.remove("hidden");
-            vendorCard.classList.add("visible");
-          });
+      document.querySelectorAll(businessTypeSelector).forEach((vendorCard) => {
+        vendorCard.classList.remove("visible");
+        vendorCard.classList.add("hidden");
       });
     });
     //
---
+++ assets/navbar-display.js
@@ -0,0 +1,26 @@
+const selectNavbar = () => {
+  const navbarBusinesses = {
+    ".nav-item.florists": ".vendor.florists",
+    ".nav-item.djs": ".vendor.djs",
+    ".nav-item.catering": ".vendor.catering",
+    ".nav-item.venues": ".vendor.venues",
+    ".nav-item.hairdressers": ".vendor.hairdressers",
+    ".nav-item.clothing": ".vendor.clothing",
+  };
+
+  for (const [selector, businessType] of Object.entries(navbarBusinesses)) {
+    const navbarBusiness = document.querySelectorAll(selector);
+    navbarBusiness.forEach((navLink) => {
+      navLink.addEventListener("click", () => {
+        document.querySelectorAll(".business").forEach((businessCard) => {
+          businessCard.classList.add("hidden");
+        });
+
+        document.querySelectorAll(businessType).forEach((vendorCard) => {
+          vendorCard.classList.remove("hidden");
+          vendorCard.classList.add("visible");
+        });
+      });
+    });
+  }
+};
